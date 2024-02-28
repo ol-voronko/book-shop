697e61d28499dc1f032378db223a000423ac649e
ol-voronko <voronyatco@gmail.com>
2024-02-28 11:31:39 +0200

    Updates

---
+++ static/js/432.4970e125.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcourse_task=self.webpackChunkcourse_task||[]).push([[432],{7432:(e,r,s)=>{s.r(r),s.d(r,{default:()=>t});var n=s(2496);function t(){return(0,n.jsx)("div",{children:(0,n.jsx)("p",{children:"Oops!Something went wrong! 404 error"})})}}}]);
+//# sourceMappingURL=432.4970e125.chunk.js.map
---
+++ static/js/884.9ba3225c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcourse_task=self.webpackChunkcourse_task||[]).push([[884],{5608:(s,e,n)=>{n.d(e,{c:()=>u});var c=n(9060),r=n(1560),t=n(784);const u=()=>{const{username:s}=(0,t.kH)(),e=(0,r.i6)();(0,c.useEffect)((()=>{s||e("/signin")}),[s])}},4884:(s,e,n)=>{n.r(e),n.d(e,{default:()=>t});var c=n(5608),r=n(2496);function t(){return(0,c.c)(),(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h2",{children:"shopping"})})}}}]);
+//# sourceMappingURL=884.9ba3225c.chunk.js.map
